65923df7763708e7af26b05b6feff139f467596e
YuchengQB <78039363+YuchengQB@users.noreply.github.com>
2022-12-20 15:40:45 +0800

    image_reco

--- GAME_四邊形.py
+++ GAME_四邊形.py
@@ -21,38 +21,45 @@ def quadrilateral():
                 print("Input Error")
     sorted(co_list)
     LL=[] #Line_List
-
+    
     line_1 = (abs(co_list[0][0]-co_list[1][0])**2 + abs(co_list[0][1]-co_list[1][1])**2)**(1/2)
     line_2 = (abs(co_list[1][0]-co_list[2][0])**2 + abs(co_list[1][1]-co_list[2][1])**2)**(1/2)
     line_3 = (abs(co_list[2][0]-co_list[3][0])**2 + abs(co_list[2][1]-co_list[3][1])**2)**(1/2)
     line_4 = (abs(co_list[3][0]-co_list[0][0])**2 + abs(co_list[3][1]-co_list[0][1])**2)**(1/2)
     line_5 = (abs(co_list[1][0]-co_list[3][0])**2 + abs(co_list[1][1]-co_list[3][1])**2)**(1/2)
     line_6 = (abs(co_list[0][0]-co_list[2][0])**2 + abs(co_list[0][1]-co_list[2][1])**2)**(1/2)
-
+    
     LL.append(line_1)
     LL.append(line_2)
     LL.append(line_3)
     LL.append(line_4)
     LL.append(line_5)
     LL.append(line_6)
-
+    
     LL.sort()
     for i in co_list:
         print("(",i[0],",",i[1],")",sep="",end=" ")
     print()
     if LL[0]==LL[1] and LL[1]==LL[2] and LL[2]==LL[3] and LL[0]==LL[3]:        
-        if LL[0] * (2**(1/2)) == LL[5] : #1:1:2(1/2)
+        if round(LL[0] * (2**(1/2)),6) == round(LL[5],6) : #1:1:2(1/2)
             print("正方形")
-        else:
-            print("菱形")
-    elif (LL[0]==LL[1] and LL[2]==LL[3]) and (LL[4]==LL[5]) and ((LL[0]**2 + LL[2]**2) == LL[5]**2): #長方形
-        print("長方形")
+    elif LL[1]==LL[2] and LL[2]==LL[3] and LL[3]==LL[4]:
+        print("菱形(平行四邊形)")
+    elif (LL[0]==LL[1] and LL[2]==LL[3]) and (LL[4]==LL[5]) and (round((LL[0]**2 + LL[2]**2),6) == round(LL[5]**2),6): #長方形
+        print("長方")
+    elif ((((LL[1]**2)-((LL[2]*0.5)**2))**(1/2))+(((LL[3]**2)-((LL[2]*0.5)**2))**(1/2)) ==LL[5] )or((((LL[1]**2)-((LL[0]*0.5)**2))**(1/2))+(((LL[3]**2)-((LL[0]*0.5)**2))**(1/2)) ==LL[5] ):
+        print("箏型")    
     elif (LL[0]==LL[1] or LL[1]==LL[2] )and LL[3]==LL[4] :
-        print("平行四邊形")
-    elif LL[0]==LL[1] and LL[2]==LL[3]:
-        print("箏形")
+        print("平行四邊形or梯形")
+    
     else:
         print("不規則")
 
 print("INTRO:\nWhat kind of quadrilateral can a Given list of four coordinates constitude")
 quadrilateral()
+
+
+
+
+
+  
